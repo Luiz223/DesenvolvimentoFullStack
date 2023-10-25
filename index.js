e7c49acbaa94170d3d9b748e476875b3b4fb4aa6
Luiz <iquesilva@outlook.com>
2023-10-24 21:51:27 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql")
+
+const app = express()
+
+//definindo handlebars como template engine
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+//pasta de arquivos estáticos como CSS, imagens
+app.use(express.static("public"))
+
+//trabalhar com dados no faotmato json
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+app.get('/', (requisicao, resposta) => {
+    resposta.render("home")
+})
+
+// conexão com mySQL
+const conn = mysql.createConnection({
+    hots: "localhost",
+    user: "root",
+    password: "root",
+    database: "nomemysql",
+    port: 3307
+})
+
+conn.connect((error) => {
+    if (error) {
+        console.log(error)
+        return
+    }
+
+    console.log("Conectado ao MySQL!")
+
+    app.listen(3000, () => {
+        console.log("servidor rodando a porta 3000!")
+    })
+})
